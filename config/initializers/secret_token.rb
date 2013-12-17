2269a79cd75635c72105bc2bdcff3397d72a947e
Himmat Shrestha <himmat.shrestha@gmail.com>
2013-12-17 22:48:17 +1100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShoppingCenter::Application.config.secret_key_base = '4d04816127d9d514e855603888f6d9887f688ab2f68cd9839e02f3fd265ae31b28872902668a5851319184c2448d930f84d3b453637837d9355e56fff7ad2601'
